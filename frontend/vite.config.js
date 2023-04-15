9fad5d11f93bea82747345d25730d9a8e078799c
it <it@xyz.com>
2023-04-15 12:39:23 +0530

    first commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "build",
+    },
+    server: {
+      port: 3000,
+      host: true
+    },
+    plugins: [react()],
+  };
+});
